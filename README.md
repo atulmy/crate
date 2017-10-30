769af4a8850742c2c46b9c30fecf0523e48cd6d4
atulmy <atul.12788@gmail.com>
2017-10-30 20:03:27 +0530

    Favicons, readme

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Crate 👕👖📦 
 
-A replica of stitchfix.com / krate.in which enables users to get monthly subscription of trendy clothes and accessories. 
+A replica of stitchfix.com / krate.in which offers users to get monthly subscription of trendy clothes and accessories. 
 
 Built with Node + Express + GraphQL + React + Redux + JWT Auth + React Storybook. 
 
 Also Server Side Rendering (SSR) + SEO friendly. 
 
-Bonus Adobe XD prototype. [Design Preview](https://xd.adobe.com/view/a662a49f-57e7-4ffd-91bd-080b150b0317/).
+Written in ES6 using Babel + Webpack.
+
+Bonus: Adobe XD prototype. [Design Preview](https://xd.adobe.com/view/a662a49f-57e7-4ffd-91bd-080b150b0317/).
 
 [WORK IN PROGRESS]
--- code/web/src/server/views/index.js
+++ code/web/src/server/views/index.js
@@ -1,18 +1,25 @@
 const index = (helmet = {}, appHtml = '', styles = '', initialState = {}) => (
 `<!doctype html>
 <html lang="en">
-  <head>
+<head>
+    <!-- Meta -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="theme-color" content="#000000">
-
     ${ helmet.title.toString() }
     
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+    
+    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     
-    <!-- Reset CSS -->
+    <!-- CSS - Reset -->
     <style type="text/css">
         html, body, div, span, applet, object, iframe,
         h1, h2, h3, h4, h5, h6, p, blockquote, pre,
@@ -59,23 +66,26 @@ const index = (helmet = {}, appHtml = '', styles = '', initialState = {}) => (
         }
     </style>
     
-    <!-- Global CSS -->
+    <!-- CSS - Global -->
     <style type="text/css">
         html, body { font-family: 'Roboto', sans-serif; background-color: #fafafa; }
         a { text-decoration: none; }
     </style>
     
-    <!-- Generated CSS -->
-    ${ styles }
-</style>
+    <!-- CSS - Generated -->
+        ${ styles }
+    </style>
 </head>
 <body>
+    <!-- App -->
     <main id="app">${ appHtml }</main>
     
+    <!-- Initial State -->
     <script>
         window.__INITIAL_STATE__ = ${ JSON.stringify(initialState) }
     </script>
     
+    <!-- JS Bundle -->
     <script type="text/javascript" src="/js/bundle.js?${ Math.random() }"></script>
 </body>
 </html>`
