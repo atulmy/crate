1c73d378610a6f66f94f2beabe4604c7006d5fdb
MarcinLoj <m.lojewski1998@gmail.com>
2023-01-10 00:07:14 +0100

    add sign in tests

---
+++ code/api/src/__tests__/testData.ts
@@ -0,0 +1,11 @@
+const userCredentials = {
+  email: "user@crate.com",
+  password: "123456",
+};
+
+const adminCredentials = {
+  email: "admin@crate.com",
+  password: "123456",
+};
+
+export { userCredentials, adminCredentials };
