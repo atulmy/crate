f5d09fc3d0c2ab20dc0abc9be34b1808dfab5ec4
MarcinLoj <m.lojewski1998@gmail.com>
2023-01-23 20:10:40 +0100

    fix women tests

--- code/web/cypress/e2e/content/Women.cy.ts
+++ code/web/cypress/e2e/content/Women.cy.ts
@@ -1,12 +1,18 @@
+import { userCredentials } from "../../../../api/src/__tests__/testData";
+
+beforeEach(() => {
+  cy.omitOnboarding();
+});
+
 describe("Women", () => {
   it("should greet with heading", function () {
-    cy.visit("/");
+    cy.visit("/women");
 
     cy.contains("Monthly crates for Women").should("be.visible");
   });
 
   it("should greet with paragraph", function () {
-    cy.visit("/");
+    cy.visit("/women");
 
     cy.contains(
       "Save time. Look great. The personal styling service customized to your fit, lifestyle & spending preferences."
@@ -14,7 +20,7 @@ describe("Women", () => {
   });
 
   it("should continue button have href attr eq to /user/signup when NOT authorized", function () {
-    cy.visit("/");
+    cy.visit("/women");
 
     cy.contains("Get Started")
       .parent()
@@ -22,10 +28,12 @@ describe("Women", () => {
   });
 
   it("should continue button have href attr eq to /user/signup when authorized", function () {
-    cy.visit("/");
+    cy.loginViaAPI(userCredentials.email, userCredentials.password).then(() => {
+      cy.visit("/women");
 
-    cy.contains("Get Subscription")
-      .parent()
-      .should("have.attr", "href", "/crates");
+      cy.contains("Get Subscription")
+        .parent()
+        .should("have.attr", "href", "/crates");
+    });
   });
 });
